e39e169a90b98c79079cb837b078004c881a2d1f
Yusup Badiev <ybadiev@gmail.com>
2026-03-25 01:00:08 -0400

    Explore getting ball to bounce

--- Assets/Ball_movment.cs
+++ Assets/Ball_movment.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public class Ball_movment : MonoBehaviour
 {
-    public float speed = 500f;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.down * speed);
     }
 }
