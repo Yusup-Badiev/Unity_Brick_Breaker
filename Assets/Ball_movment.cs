b0da5c29410885b28427fef2c0f88a3fb77509e8
Yusup Badiev <ybadiev@gmail.com>
2026-03-26 22:53:25 -0400

    Attempt to reflect ball off walls

--- Assets/Ball_movment.cs
+++ Assets/Ball_movment.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class Ball_movment : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 2f;
+    public Vector2 currentDirection = Vector2.down;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Running Start method");
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.AddForce(Vector2.down * speed);
+    }
+
+    void Update()
+    {
+        currentDirection = GetComponent<Rigidbody2D>().linearVelocity.normalized;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected with 2d");
+        Debug.Log("Collision detected with 2d: " + collision.gameObject.name);
+        Vector2 reflectedDirection = Vector2.Reflect(currentDirection, currentDirection);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = reflectedDirection * speed;
     }
 }
